106dabb6c3f1311471786048945b272d11d85ca4
n0taaryav <128344052+n0taaryav@users.noreply.github.com>
2023-06-10 13:00:34 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,14 +1,28 @@
+import random
 
-powers = [3, 8, 9, 7]
+def number_guessing_game():
+    random_number = random.randint(1, 100)
+    attempts = 5
 
-mini, maxi = 0, 0
+    print("Welcome to the Number Guessing Game!")
+    print("I'm thinking of a number between 1 and 100.")
+    print(f"You have {attempts} attempts to guess it.")
 
-for power in powers:
-	if mini == 0 and maxi == 0:
-		mini, maxi = powers[0], powers[0]
-		print(mini, maxi)
-	else:
-		mini = min(mini, power)
-		maxi = max(maxi, power)
-		print(mini, maxi)
-		
+    while attempts > 0:
+        guess = int(input("Enter your guess: "))
+
+        if guess < random_number:
+            print("Too low!")
+        elif guess > random_number:
+            print("Too high!")
+        else:
+            print("Congratulations! You guessed the number correctly!" + "It was "+ random_number + "!")
+            return
+
+        attempts -= 1
+        print(f"Attempts left: {attempts}")
+
+    print(f"\nGame over! You ran out of attempts. The number was {random_number}.")
+
+
+number_guessing_game()
